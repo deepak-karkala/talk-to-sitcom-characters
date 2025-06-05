1f04c8762d32dd4fd7cf27b95785292e806ec344
Deepak Karkala <dkarkala01@gmail.com>
2025-06-05 16:33:07 +0530

    correcting layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
